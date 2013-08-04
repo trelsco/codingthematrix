d2d79fe0887d8e5fdd8e2344f27e7be58db66146
dyansane-sisk <t0piary@lavabit.com>
2013-08-04 03:00:35 -0700

    add comments to inverse_lab

--- inverse_index_lab.py
+++ inverse_index_lab.py
@@ -22,11 +22,11 @@ def makeInverseIndex(strlist):
     Note that to test your function, you are welcome to use the files stories_small.txt
       or stories_big.txt included in the download.
     """
-    dico = dict()
-    for i,j in enumerate(strlist):
-        for k in j.split():
-            if k in dico: dico[k].add(i)
-            else: dico[k] = {i}
+    dico = dict()  # create empty dictionary
+    for i,j in enumerate(strlist):  # for every (doc_id, document) pair
+        for k in j.split():     # split the document into words and traverse
+            if k in dico: dico[k].add(i)   # if word is key , add doc_id to its value set
+            else: dico[k] = {i}   # otherwise create a key for new word and add doc_id
     return dico
 
     
@@ -36,10 +36,10 @@ def orSearch(inverseIndex, query):
     Input: an inverse index, as created by makeInverseIndex, and a list of words to query
     Output: the set of document ids that contain _any_ of the specified words
     """
-    if query == []: return set()
+    if query == []: return set()  
     else:
-        S = set()
-        for x in query:
+        S = set()    
+        for x in query:  
             S.update(inverseIndex[x])
         return S
 
