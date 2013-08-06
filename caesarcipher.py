4e24457a93bd3710bf3560b936b330cd821e03fd
dyansane-sisk <t0piary@lavabit.com>
2013-08-06 13:46:40 -0700

    Update caesarcipher.py

--- caesarcipher.py
+++ caesarcipher.py
@@ -4,10 +4,10 @@ import string
 ### originally created while taking edX 6.00x from MIT; cleaned up after learning more on comprehensions
 
 def caesarCipher(shift):
-  L = string.ascii_lowercase
-  U = string.ascii_uppercase
-  cipher = {U[i]:(U[shift:]+U[:shift])[i] for i in range(26)}
-  cipher.update({L[i]:(L[shift:]+L[:shift])[i] for i in range(26)})
+  L = string.ascii_lowercase  # lowercase abc's
+  U = string.ascii_uppercase  # uppercase abc's
+  cipher = {U[i]:(U[shift:]+U[:shift])[i] for i in range(26)}  # create dictonary , iterating the shifted alphabet to make values
+  cipher.update({L[i]:(L[shift:]+L[:shift])[i] for i in range(26)}) # update dictionary w/ lowercase mapping
   return cipher
   
 def putCoder(text,coder):
@@ -15,10 +15,11 @@ def putCoder(text,coder):
   U = string.ascii_uppercase
   res = ''
   for i in text:
-    if i not in U and i not in L: res += i
+    if i not in U and i not in L: res += i    
     else: res += coder[i]
   return res
   
+# wrapper function for putCoder, functional style 
 def applyShift(text,shift):
   return putCoder(text,caesarCipher(shift))
   
